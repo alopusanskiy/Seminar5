52a06e71a0a2b2c194b565b1f0307ac3f11a0e00
«Ales» <alopusansky@gmail.com>
2023-06-28 14:23:38 +0300

    added solution in the Sem5 task_002

---
+++ task_002/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(0, 16); // диапазон маленький, что бы было удобно проверять правильность работы программы.
+    }
+    System.Console.Write("[ " + string.Join(", ", array) + " ]");
+    return array;
+}
+
+// void PrintArray(int[] arr)
+// {
+//     System.Console.Write("[ " + string.Join(", ", arr) + " ]");
+// }
+
+
+void OddNum(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    System.Console.WriteLine(sum);
+}
+
+OddNum(FillArray(5));
+
+
+
+
